fa0997f24102c8335fc048260c2c62475665a468
kjgorman <kieran@kjgorman.com>
2016-05-12 17:58:27 +1200

    smush iteration state into an object

--- cream.py
+++ cream.py
@@ -6,49 +6,56 @@ from partitions import choose
 import genetic  as g
 import sampling as s
 
+class IterationState:
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
 if __name__ == '__main__':
     width = 1536
-    # pop_fouriers = [ s.sample_fourier(width) for _ in xrange(0, 5000) ]
     pop_freqs = s.sample_fourier(50000)
-
-    # create 100 random subsamples
-    population = [ choose(pop_freqs, width) for _ in xrange(0, 500) ]
-    #population = choose(pop_fouriers, 100)
-    base_line = 30 * np.cos((1.0 / 64) * np.arange(width))
     x_axis = np.arange(width)
+
+    figure = plt.figure()
+    base_line = 30 * np.cos((1.0 / 64) * np.arange(width))
+
+    state = IterationState(
+        population = [ choose(pop_freqs, width) for _ in xrange(0, 500) ],
+        base_line = base_line,
+        delta = 10000,
+        previous = 10000,
+        line = plt.plot([], [], x_axis, base_line)[0],
+        epoch = 0)
 
     def best_error(pop):
         return g.evaluate(pop, base_line)[0][0]
 
-    delta = 10000
-    previous = 10000
-
-    figure = plt.figure()
-    line, base = plt.plot([], [], x_axis, base_line)
-
     plt.xlim(0, width)
     plt.ylim(-200, 200)
 
-    def update_line(frame):
-        # this is stupid -- should pack it into an object
-        global population
-        global delta
-        global base_line
-        global line
-        global previous
-        epoch = 0
+    def update_line(frame, state):
+        current = 0
 
-        while epoch < 100:
-            population = g.generation(population, base_line)
-            best = best_error(population)
+        while current < 100:
+            state.population = g.generation(state.population, state.base_line)
+            best = best_error(state.population)
 
-            if epoch % 100 == 0:
-                print "epoch %s \t RMSE: %s \t delta: %s" % (epoch, best, delta)
-                delta = previous - best
-                previous = best
-            epoch += 1
+            if state.epoch % 100 == 0:
+                print "epoch %s \t RMSE: %s \t delta: %s" % (state.epoch, best, state.delta)
+                state.delta = state.previous - best
+                state.previous = best
 
-        line.set_data(np.arange(width), g.evaluate(population, base_line)[0][1])
+            state.epoch += 1
+            current += 1
 
-    anim = animation.FuncAnimation(figure, update_line, 50, interval=5)
+        state.line.set_data(
+            np.arange(width), g.evaluate(state.population, state.base_line)[0][1])
+
+    anim = animation.FuncAnimation(
+        figure,
+        update_line,
+        50,
+        interval=5,
+        fargs=(state,))
+
     plt.show()
